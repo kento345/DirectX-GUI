06e55ab32fa38923ae2a72d1bc8fe89fbdadeb03
K\Kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2026-06-09 18:06:19 +0900

    CommandList.cppの途中

--- directx/Command_List.cpp
+++ directx/Command_List.cpp
@@ -1,1 +1,22 @@
 #include "Command_List.h"
+#include<cassert>
+
+//コマンドリスト生成
+bool Command_List::create(const Command_Allocator& command_Allocator)noexcept {
+	//コマンドリストの作成
+	const auto hr = Device::instance().get()->CreateCommandList(
+		0, command_Allocator.getType(), command_Allocator.get(), nullptr, IID_PPV_ARGS(&commandList_)
+	);
+	if (FAILED(hr)) {
+		assert(false && "コマンドリストの作成に失敗しました");
+		return false;
+	}
+	//コマンドリストの初期化状態設定
+	commandList_->Close();
+	return true;
+}
+
+//コマンドリストのリセット
+void Command_List::reset(const Command_Allocator& command_Allocator)noexcept {
+
+}
